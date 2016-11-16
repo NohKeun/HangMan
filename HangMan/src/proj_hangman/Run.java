bdbec7c1cdddb8cab12ee6d5cdd1a6b926bb3058
NK_lenovo <NK_lenovo@NK-lenovo>
2016-11-16 12:11:39 +0900

    11.16일 _ 까지 완료

--- HangMan/src/proj_hangman/Run.java
+++ HangMan/src/proj_hangman/Run.java
@@ -1,9 +1,59 @@
 package proj_hangman;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Run {
 
-	Scanner scanner = new Scanner(System.in);
+	private Scanner scanner = new Scanner(System.in);
+	
+	private Run run;
+	
+	private String[] words= {"apple", "potato", "people"};	
+	private String gameWord;
+	
+	private Random random = new Random();
+	
+	
+	public void menu() {
+		int num;
+		System.out.println("게임시작 : 1, 종료 : 2");
+		num = scanner.nextInt();
 
+		switch(num) {
+		case 1:
+			gameStart();
+			break;
+		case 2:
+			gameEnd();
+			break;	
+		}
+	}
+	
+	private void gameStart() {
+		gameRun();
+	}
+	
+	private void gameEnd() {
+		//나중에 구현
+		System.out.println("게임 종료!");
+	}
+	
+	private void error() {
+		
+		System.out.println("에러");
+	}
+	
+	private void gameRun() {
+		int num = random.nextInt(words.length);
+		
+		gameWord = words[num];
+		
+		System.out.println(gameWord);
+		
+		for(int i = 0; i<words[num].length(); i++) {
+			System.out.print("_ ");
+		}
+	}
+	
 }
--- HangMan/src/proj_hangman/SaveWords.java
+++ HangMan/src/proj_hangman/SaveWords.java
@@ -1,12 +1,23 @@
 package proj_hangman;
 
+import java.util.*;
+import java.io.*;
+
 public class SaveWords {
 
 	private String fileName;
+	private Scanner scanner = new Scanner(System.in);
+	private HashMap<String, String> map = new HashMap<String, String>();
 	
 	public SaveWords(String fileName) {
 		this.fileName = fileName;
 	}
 	
+	private void insert() {
+		System.out.println("단어 입력 :");
+		String name1 = scanner.next();
+				
+		
+	}
 	
 }
